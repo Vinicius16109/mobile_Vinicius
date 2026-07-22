8a3ed55db3c64aeff810df386314126d6fe68ced
Vinicius16109 <vinicius.golinski@escola.pr.gov.br>
2026-07-22 14:29:21 -0400

    Create ex02.js

---
+++ lista1/ex02.js
@@ -0,0 +1,7 @@
+const base = 10;
+const altura = 5;
+const area = base * altura;
+const perímetro = (base * 2) + (altura * 2); 
+
+console.log(`Área do retângulo: ${area}`);
+console.log(`Perímetro do retângulo: ${perímetro}`);
