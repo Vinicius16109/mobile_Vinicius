d50111ca1ae09c3ef7f30025033691a568c67899
Vinicius16109 <vinicius.golinski@escola.pr.gov.br>
2026-07-22 13:57:22 -0400

    Create ex02.js

---
+++ aula1/ex02.js
@@ -0,0 +1,9 @@
+const somar = (a, b) => a + b;
+const subtrair = (a, b) => a - b;
+const multiplicar = (a, b) => a * b;
+const dividir = (a, b) => a / b;
+
+console.log("Soma:", somar(10, 5));
+console.log("Subtração:", subtrair(10, 5));
+console.log("Multiplicação:", multiplicar(10, 5));
+console.log("Divisão:", dividir(10, 5));
